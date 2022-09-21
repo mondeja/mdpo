126587c99d5e8861a0fd2297af96a242776c7a56
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-09-21 05:09:09 +0200

    Fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -150,7 +150,10 @@ def _wrap_location_comment(filepath, rest):
     if len(result) > 75:
         result += f'\n#: {number}'
     else:
-        result += f' {number}'
+        if len(result) + len(number) > 75:
+            result += f'\n#: {number}'
+        else:
+            result += f' {number}'
     result += f' {context}'
     return result
 
