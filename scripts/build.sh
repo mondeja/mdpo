3b3d9fb2c4b22b62cb4be14e0f6933ce828440f5
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-11-11 12:34:12 +0100

    Distribute auto contained binaries

---
+++ scripts/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -ex
+
+rm -rf build dist
+export TMPDIR=/var/tmp/
+
+pyinstaller -y --onefile \
+    --optimize 2 \
+    --name md2po \
+    --copy-metadata mdpo \
+    src/mdpo/md2po/__main__.py
+pyinstaller -y --onefile \
+    --optimize 2 \
+    --name po2md \
+    --copy-metadata mdpo \
+    src/mdpo/po2md/__main__.py
+pyinstaller -y --onefile \
+    --optimize 2 \
+    --name md2po2md \
+    --copy-metadata mdpo \
+    src/mdpo/md2po2md/__main__.py
+pyinstaller -y --onefile \
+    --optimize 2 \
+    --name mdpo2html \
+    --copy-metadata mdpo \
+    src/mdpo/mdpo2html/__main__.py
