e6789277dc2fe623d55fd3dd4c8f700305e372ab
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-11-16 20:18:47 +0100

    Faster builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,22 +4,23 @@ set -ex
 
 rm -rf build dist
 export TMPDIR=/var/tmp/
+export PYI_LOG_LEVEL=WARN
 
 pyinstaller -y --onefile \
     --optimize 2 \
     --name md2po \
     --copy-metadata mdpo \
-    src/mdpo/md2po/__main__.py
+    src/mdpo/md2po/__main__.py &
 pyinstaller -y --onefile \
     --optimize 2 \
     --name po2md \
     --copy-metadata mdpo \
-    src/mdpo/po2md/__main__.py
+    src/mdpo/po2md/__main__.py &
 pyinstaller -y --onefile \
     --optimize 2 \
     --name md2po2md \
     --copy-metadata mdpo \
-    src/mdpo/md2po2md/__main__.py
+    src/mdpo/md2po2md/__main__.py &
 pyinstaller -y --onefile \
     --optimize 2 \
     --name mdpo2html \
